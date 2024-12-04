fbc0bd5ce71f75a8d3d8d8915db2b3e067016b38
Pepe <pepe@yandex.ru>
2024-12-04 06:57:56 +0300

    Вторая половина кода написана

--- vvpd4.py
+++ vvpd4.py
@@ -9,4 +9,10 @@ for i in range(m):
     lst.append([])
     for j in range(n):
         lst[i].append(random.randint(-1000,1000))
-
+for i in range(len(lst)):
+    for j in range(1, len(lst[i])):
+        if j < i and lst[i][j] < 0:
+            c += 1
+for i in range(len(lst)):
+    print(lst[i])
+print(c)
